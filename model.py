5d182c01aa3b51fdd4d71a231aa06bf4dc4ab8e8
AlexCh069 <alex.ch.o573@gmail.com>
2025-03-28 13:14:47 -0500

    xgboost models

--- model.py
+++ model.py
@@ -2,12 +2,13 @@ from imblearn.combine import SMOTETomek
 import pandas as pd
 import logging
 import mlflow
+import numpy as np
 from utils.utils import ExtractionData 
 import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix, accuracy_score
-
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, roc_auc_score, average_precision_score
+import xgboost as xgb
 
 # print(os.getcwd())  # Visualizar nuestro posicionamiento en el directorio
 
@@ -26,40 +27,50 @@ data_resample = util.smoteenn_resample(data,'Exited')
 x_smot_train, x_smot_test, y_smot_train, y_smot_test = util.split_data(data_resample) # Resampleo de data
 
 # Iniciar y registrar un experimento
-with mlflow.start_run(run_name="rf_smoteenn"):
+with mlflow.start_run(run_name="xgb_smoteenn"):
 
-    rf = RandomForestClassifier(bootstrap=True, random_state=69)
+    # Model Training --------------------------------------------------------------------------------
+    rf = xgb.XGBClassifier(use_label_encoder = False, eval_metric = 'logloss')
 
     param_grid = {
-    'n_estimators': [50, 100, 150],  # Número de árboles
-    'max_depth': [6, 10, 15],  # Profundidad máxima
-    'min_samples_split': [2, 3, 5],  # Mínimo de muestras para dividir un nodo
-    'min_samples_leaf': [2, 3, 4],  # Mínimo de muestras en una hoja
-    'max_features': ['sqrt', 'log2'],  # Número de características a considerar en cada división
-}
+    'n_estimators': [100, 150, 200, 250, 300],       # Número de árboles
+    'learning_rate': np.arange(0.01, 0.051, 0.03), # Tasa de aprendizaje
+    'max_depth': [3, 5, 7],            # Profundidad máxima de los árboles
+    'subsample': [0.05, 0.8, 1.0],           # Fracción de datos usada por árbol
+    'colsample_bytree': [0.4, 0.6, 0.8, 1.0]     # Fracción de features usadas por árbol
+    }
 
 
-    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=2, verbose=2, scoring='accuracy')
+
+    grid_search = GridSearchCV(estimator=rf, 
+                               param_grid=param_grid, 
+                               cv=2, 
+                               verbose=2, 
+                               n_jobs=-1,
+                               scoring='accuracy')
+    
     grid_search.fit(x_smot_train, y_smot_train)
-
+    # ----------------------------------------------------------------------------------------------
+    
+    # Resutls --------------------------------------------------------------------------------------
     print("Mejores parámetros:", grid_search.best_params_)
     mlflow.log_params(grid_search.best_params_)
     
     print("Mejor puntuación de validación cruzada:", grid_search.best_score_)
     mlflow.log_metric("acuracy_train",grid_search.best_score_)
+    # ----------------------------------------------------------------------------------------------
 
-
+    # Model tracking -------------------------------------------------------------------------------
     best_rf2 = grid_search.best_estimator_
-    
     mlflow.sklearn.log_model(best_rf2, "model_1")
+    # ----------------------------------------------------------------------------------------------
 
     # Predicciones en el conjunto de prueba
     y_pred = best_rf2.predict(x_smot_test)
 
-    # Generar la matriz de confusión
+    # Generar la matriz de confusión ---------------------------------------------------------------
     cm = confusion_matrix(y_smot_test, y_pred)
     # Convertir a un diccionario para almacenarla como un parámetro
-
     cm_notation = ['TN','FP','FN','TP']
     cm_dict = {
         cm_notation[0]: cm[0,0],
@@ -68,15 +79,28 @@ with mlflow.start_run(run_name="rf_smoteenn"):
         cm_notation[3]: cm[1,1]
         }
     mlflow.log_params(cm_dict)
+    # ----------------------------------------------------------------------------------------------
 
+    # Exactitud, Precision, Sensibilidad y Exhaustividad -------------------------------------------
+    tn, fp, fn, tp = cm.ravel()
 
-    # Calcular precisión
     accuracy = accuracy_score(y_smot_test, y_pred)
-    print("Precisión en el conjunto de prueba:", accuracy)
-    mlflow.log_metric("accuracy_test", accuracy)
+    precision = precision_score(y_smot_test, y_pred)
+    recall = recall_score(y_smot_test, y_pred)
+    specificity = tn/(tn + fp)
+    auc_roc = roc_auc_score(y_smot_test, best_rf2.predict_proba(x_smot_test)[:, 1])
+    auc_pr = average_precision_score(y_smot_test, best_rf2.predict_proba(x_smot_test)[:,1])
+    
+    mlflow.log_metric("accuracy_test", round(accuracy,5))
+    mlflow.log_metric('precision_test', round(precision,5))
+    mlflow.log_metric('recall_test', round(recall,5))
+    mlflow.log_metric('specificity_test', round(specificity,5))
+    mlflow.log_metric('auc_roc_test', round(auc_roc,5))     # Que tan bien separa ambas clases
+    mlflow.log_metric('auc_pr_test', round(auc_pr,5))       # Mejor adaptacion a la clase positiva
+
+    # ---------------------------------------------------------------------------------------------
 
     print(f"default artifact location: '{mlflow.get_artifact_uri()}'")
-
 
 
     # correr la ui: mlflow ui --backend-store-uri "file://C:/Users/Ares/Documents/LIBROS/PLATZI CURSOS/CIENCIA DE DATOS/MACHINE LEARNING/laboratorio-machine-learning/ml_experiments"
